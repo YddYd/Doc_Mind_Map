76886c4b0ae806d32fc01cffc7c55437a7b989c5
YddYd <58283598+YddYd@users.noreply.github.com>
2025-10-26 02:35:41 -0400

    Delete Strucural Cooperaation directory

--- Strucural Cooperaation/service_worker.js
+++
@@ -1,8 +0,0 @@
-// 打开 Side Panel
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.sidePanel.setOptions({ path: "sidepanel.html", enabled: true });
-});
-
-chrome.action?.onClicked?.addListener(async (tab) => {
-  await chrome.sidePanel.open({ tabId: tab.id });
-});
